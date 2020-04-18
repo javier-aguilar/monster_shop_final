bd55164d9de9de6186220ef582b1cbc046a6819f
Javier Aguilar <jvr.agx@gmail.com>
2020-04-18 14:19:16 -0600

    Add discount index page

--- spec/features/discounts/index_spec.rb
+++ spec/features/discounts/index_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe 'Show Discount Page' do
+RSpec.describe 'Index Discount Page' do
   describe 'As a merchant employee' do
     before do
       megan = Merchant.create!(name: 'Megans Marmalades', address: '123 Main St', city: 'Denver', state: 'CO', zip: 80218)
@@ -8,7 +8,9 @@ RSpec.describe 'Show Discount Page' do
       megan.items.create!(name: 'Ogre', description: "I'm an Ogre!", price: 20, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 5 )
       megan.items.create!(name: 'Giant', description: "I'm a Giant!", price: 50, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 3 )
       brian.items.create!(name: 'Hippo', description: "I'm a Hippo!", price: 50, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 3 )
-      @discount = megan.discounts.create!(code: "50OFF", description: "50% off 10 items or more", discount: 50, number_of_items: 10)
+      @discount1 = megan.discounts.create!(code: "50OFF", description: "50% off 10 items or more", discount: 50, number_of_items: 10)
+      @discount2 = megan.discounts.create!(code: "60OFF", description: "60% off 15 items or more", discount: 60, number_of_items: 15)
+      @discount3 = brian.discounts.create!(code: "SUMMER10", description: "10% off 10 items or more", discount: 10, number_of_items: 10)
 
       employee = User.create(name: "Mike Dao",
            address: "1765 Larimer St",
@@ -27,17 +29,27 @@ RSpec.describe 'Show Discount Page' do
 
        click_button "Log In"
     end
-    it 'I can visit /discounts/:id to see discount information' do
+    it 'I can see a list of discounts' do
 
-      visit "/merchant/discounts/#{@discount.id}"
+      visit merchant_discounts_path
 
-      within(".discount-#{@discount.id}") do
+      within(".discount-#{@discount1.id}") do
         expect(page).to have_content("Code: 50OFF")
         expect(page).to have_content("Description: 50% off 10 items or more")
         expect(page).to have_content("Discount: 50%")
         expect(page).to have_content("Number of Items needed: 10")
       end
 
+      within(".discount-#{@discount2.id}") do
+        expect(page).to have_content("Code: 60OFF")
+        expect(page).to have_content("Description: 60% off 15 items or more")
+        expect(page).to have_content("Discount: 60%")
+        expect(page).to have_content("Number of Items needed: 15")
+      end
+
+      expect(page).to have_no_content("Code: SUMMER10")
+      expect(page).to have_no_content("Description: 10% off 10 items or more")
+      expect(page).to have_no_content("Discount: 10%")
     end
   end
 end
