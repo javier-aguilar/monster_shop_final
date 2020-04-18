4fff7cb2f368c11eb1a29b8b8e91631c27b18565
Javier Aguilar <jvr.agx@gmail.com>
2020-04-18 11:32:47 -0600

    Update have_link references in spec tests

---
+++ spec/features/discounts/new_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe 'New Discount Creation' do
+  describe 'As a merchant employee' do
+    before do
+      
+    end
+    it 'I can visit /discounts/new to fill out a form to create a discount' do
+
+      visit "/discounts/new"
+
+    end
+  end
+end
