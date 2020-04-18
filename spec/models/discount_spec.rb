de4962adf31d777f40906ebb6b05944d14689a10
Javier Aguilar <jvr.agx@gmail.com>
2020-04-18 16:49:11 -0600

    Add discout model spec test

---
+++ spec/models/discount_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe Discount do
+  describe 'Relationships' do
+    it {should belong_to :merchant}
+  end
+
+  describe 'Validations' do
+    it {should validate_presence_of :code}
+    it {should validate_presence_of :description}
+    it {should validate_presence_of :discount}
+    it {should validate_presence_of :number_of_items}
+  end
+
+end
